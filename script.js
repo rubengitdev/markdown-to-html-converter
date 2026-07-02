7ee89ea16e8ecdc2e903881a07e2dfa9d23c6210
rubengitdev <rubencahyadi504@gmail.com>
2026-07-02 18:35:03 +0700

    Finishing Converter for Headings, images, links, bold, italic, and quotes

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-function convertMarkdown() {}
+const markdownInput = document.querySelector("#markdown-input");
+const htmlOutput = document.querySelector("#html-output");
+const preview = document.querySelector("#preview");
+
+function convertMarkdown() {
+  let html = markdownInput.value;
+
+  // headings
+  html = html.replace(/^\s*### (.+)$/gm, "<h3>$1</h3>");
+  html = html.replace(/^\s*## (.+)$/gm, "<h2>$1</h2>");
+  html = html.replace(/^\s*# (.+)$/gm, "<h1>$1</h1>");
+
+  // images
+  html = html.replace(/!\[(.*?)\]\((.*?)\)/g, '<img alt="$1" src="$2">');
+
+  // bold
+  html = html.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
+
+  // italic text
+  html = html.replace(/\*(.+?)\*/g, "<em>$1</em>");
+
+  // Italic _text_
+  html = html.replace(/_(.+?)_/g, "<em>$1</em>");
+
+  // links
+  html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+
+  // quotes
+  html = html.replace(/^\s*> (.+)$/gm, "<blockquote>$1</blockquote>");
+
+  return html;
+}
+
+markdownInput.addEventListener("input", () => {
+  const html = convertMarkdown();
+
+  htmlOutput.textContent = html;
+  preview.innerHTML = html;
+});
